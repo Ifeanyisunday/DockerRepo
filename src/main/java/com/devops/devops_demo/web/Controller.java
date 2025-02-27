2b14b4b8a30d98c986ac73c2a1da6e743d265578
ifeanyisunday <sundayjnr10@gmail.com>
2025-02-27 12:38:03 +0100

    first commit

---
+++ src/main/java/com/devops/devops_demo/web/Controller.java
@@ -0,0 +1,14 @@
+package com.devops.devops_demo.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
